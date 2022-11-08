d8d112cf248c45624b4b6b06b913bdbaed13fb5c
Peter Durham <peterdurham@protonmail.com>
2022-11-08 08:55:44 -0800

    added blocks list and add block component

--- pages/add-block.js
+++ pages/add-block.js
@@ -1,21 +1,148 @@
+import { useState } from "react";
 import Link from "next/link";
+import Button from "../components/Button";
+import TextInput from "../components/TextInput";
+
+import styled from "styled-components";
+
+const CreateBlockWrapper = styled.div`
+  box-shadow: rgb(145 158 171 / 20%) 0px 0px 2px 0px,
+    rgb(145 158 171 / 12%) 0px 12px 24px -4px;
+  border-radius: 16px;
+  padding: 24px;
+  margin: 24px;
+`;
 
 const AddBlock = ({ blocks, blockBuilding }) => {
-  if (!blockBuilding.parent) {
-    return (
-      <div>
-        <h1>Select a parent block to build on</h1>
-        <Link href="/blocks">Goto blocks page</Link>
-      </div>
-    );
-  }
+  const [editing, setEditing] = useState(null);
+  const [blockData, setBlockData] = useState({
+    height: "",
+    parent: "",
+    twitterURL: "",
+  });
+  const [proofs, setProofs] = useState([
+    { amount: "", twitterURL: "", miner: "" },
+  ]);
+
+  // if (!blockBuilding.parent) {
+  //   return (
+  //     <div>
+  //       <h1>Select a parent block to build on</h1>
+  //       <Link href="/blocks">Goto blocks page</Link>
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div>
-      <h1>Add block Page</h1>
-      <p>Building on block {blockBuilding.parentBuilder}</p>
-      <p>New Block Height: {Number(blockBuilding.height) + 1}</p>
-    </div>
+    // <div>
+    //   <h1>Add block Page</h1>
+    //   <p>Building on block {blockBuilding.parentBuilder}</p>
+    //   <p>New Block Height: {Number(blockBuilding.height) + 1}</p>
+    // </div>
+    <CreateBlockWrapper>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "300px",
+          margin: "0 auto",
+        }}
+        onSubmit={(e) => onSubmitBlock(e)}
+      >
+        <h2>Enter block:</h2>
+
+        <TextInput
+          type="text"
+          name="height"
+          label="Height"
+          value={blockData.height}
+          onChange={(e) =>
+            setBlockData({
+              ...blockData,
+              height: e.target.value,
+            })
+          }
+        />
+        <TextInput
+          type="text"
+          name="parent"
+          label="Parent"
+          value={blockData.parent}
+          onChange={(e) =>
+            setBlockData({
+              ...blockData,
+              parent: e.target.value,
+            })
+          }
+        />
+        <TextInput
+          type="text"
+          name="twitterURL"
+          label="Twitter URL"
+          value={blockData.twitterURL}
+          onChange={(e) =>
+            setBlockData({
+              ...blockData,
+              twitterURL: e.target.value,
+            })
+          }
+        />
+        <h2>Proofs:</h2>
+        <div>
+          <div>Amount | Twitter URL</div>
+          {proofs.map((proof, index) => {
+            return (
+              <div style={{ display: "flex", marginBottom: "8px" }} key={index}>
+                <div
+                  style={{
+                    display: "flex",
+                    marginRight: "8px",
+                    alignItems: "center",
+                  }}
+                >
+                  {index + 1}.
+                </div>
+
+                <TextInput
+                  type="text"
+                  name="amount"
+                  showLabel={false}
+                  onChange={(e) => {
+                    const updatedProofs = [...proofs];
+                    updatedProofs[index].amount = e.target.value;
+                    setProofs(updatedProofs);
+                  }}
+                  value={proofs[index].amount}
+                />
+                <TextInput
+                  type="text"
+                  name="twitterURL"
+                  showLabel={false}
+                  onChange={(e) => {
+                    const updatedProofs = [...proofs];
+                    updatedProofs[index].twitterURL = e.target.value;
+                    updatedProofs[index].miner =
+                      e?.target?.value?.split("/")[3] || "";
+                    setProofs(updatedProofs);
+                  }}
+                  value={proofs[index].twitterURL}
+                />
+                {index === proofs.length - 1 && (
+                  <Button
+                    onClick={() =>
+                      setProofs([...proofs, { amount: "", twitterURL: "" }])
+                    }
+                  >
+                    +
+                  </Button>
+                )}
+              </div>
+            );
+          })}
+        </div>
+        <Button variation="primary">Add Block</Button>
+      </form>
+    </CreateBlockWrapper>
   );
 };
 
--- pages/blocks.js
+++ pages/blocks.js
@@ -1,47 +1,179 @@
+import { useState } from "react";
 import Button from "../components/Button";
+import TextInput from "../components/TextInput";
 import Link from "next/link";
+import axios from "axios";
+import styled from "styled-components";
+
+const BlockWrapper = styled.div`
+  color: rgb(33, 43, 54);
+  box-shadow: rgb(145 158 171 / 20%) 0px 0px 2px 0px,
+    rgb(145 158 171 / 12%) 0px 12px 24px -4px;
+  border-radius: 16px;
+  padding: 24px;
+  margin-bottom: 12px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Blocks = ({ blocks, setBlockBuilding }) => {
+  const [editing, setEditing] = useState(null);
+  const [blockData, setBlockData] = useState({
+    height: "",
+    parent: "",
+    twitterURL: "",
+  });
+  const [proofs, setProofs] = useState([
+    { amount: "", twitterURL: "", miner: "" },
+  ]);
+
+  // Create
+  const onSubmitBlock = async (e) => {
+    e.preventDefault();
+    const builder = blockData?.twitterURL?.split("/")[3] || "";
+
+    await axios.post("/api/blocks", {
+      height: blockData.height,
+      builder: builder,
+      twitterURL: blockData.twitterURL,
+      proofs,
+    });
+
+    // height.value = "";
+    // builder.value = "";
+    // twitterURL.value = "";
+    // getBlocks();
+  };
+
+  // Update
+  const onSubmitEdits = async (e, id) => {
+    e.preventDefault();
+    const { height, builder, twitterURL } = e.target;
+    await axios.post(`/api/blocks/update/${id}`, {
+      height: height.value,
+      builder: builder.value,
+      twitterURL: twitterURL.value,
+    });
+    setEditing(null);
+    getBlocks();
+  };
+
+  // Delete
+  const deleteBlock = async (blockToDelete) => {
+    // await axios({
+    //   method: "DELETE",
+    //   url: "/api/blocks/",
+    //   data: {
+    //     id: blockToDelete,
+    //   },
+    // });
+    // await getBlocks();
+  };
+
   return (
     <div>
       <h1>Blocks Page</h1>
-      {blocks
-        .sort((a, b) => b.height - a.height)
-        .map((block, index) => {
-          console.log("block", block);
-          return (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                border: "1px solid grey",
-                borderRadius: "4px",
-                marginBottom: "8px",
-                padding: "16px",
-              }}
-              key={index}
-            >
-              <div>
-                <div>{block.height}: </div>
-                <div>{block.builder}</div>
-              </div>
-              <Link href={`/add-block`}>
-                <Button
-                  onClick={() =>
-                    setBlockBuilding({
-                      parent: block.parent,
-                      parentBuilder: block.parentBuilder,
-                      height: block.height + 1,
-                    })
-                  }
-                >
-                  Build on this Block
-                </Button>
-              </Link>
+
+      <div className="DataOutput">
+        {blocks
+          .sort((a, b) => b.height - a.height)
+          .map((block, index) => (
+            <div key={index}>
+              {editing !== block._id ? (
+                <BlockDisplay
+                  block={block}
+                  setEditing={setEditing}
+                  deleteBlock={deleteBlock}
+                />
+              ) : (
+                <BlockEdit
+                  block={block}
+                  onSubmitEdits={onSubmitEdits}
+                  setEditing={setEditing}
+                  setBlockData={setBlockData}
+                />
+              )}
             </div>
-          );
-        })}
+          ))}
+      </div>
     </div>
+  );
+};
+
+const BlockDisplay = ({ block, setEditing, deleteBlock }) => {
+  return (
+    <BlockWrapper key={block._id}>
+      <div>
+        <div>
+          <div>
+            <span>Height:</span>
+            {block.height}
+          </div>
+          <div>
+            <span>Builder:</span>
+            {block.builder}
+          </div>
+          <div>
+            <span>Twitter URL:</span>
+            {block.twitterURL}
+          </div>
+        </div>
+        <div style={{ display: "flex" }}>
+          <Button onClick={() => setEditing(block._id)}>Edit</Button>
+          <Button onClick={() => deleteBlock(block._id)}>Delete</Button>
+        </div>
+      </div>
+
+      <div>
+        <Button variation="primary">Build on this block</Button>
+      </div>
+    </BlockWrapper>
+  );
+};
+
+const BlockEdit = ({ block, onSubmitEdits, setEditing, setBlockData }) => {
+  return (
+    <BlockWrapper key={block._id} className="DataOutput__editing">
+      <form onSubmit={(e) => onSubmitEdits(e, block._id)}>
+        <div className="DataOutput__editing--option">
+          <TextInput
+            type="text"
+            name="height"
+            labelText="Height"
+            defaultValue={block.height}
+            value={block.height}
+          />
+        </div>
+        <div className="DataOutput__editing--option">
+          <TextInput
+            type="text"
+            name="builder"
+            labelText="Builder"
+            defaultValue={block.builder}
+            value={block.builder}
+          />
+        </div>
+        <div className="DataOutput__editing--option">
+          <TextInput
+            type="text"
+            name="twitterURL"
+            labelText="Twitter URL"
+            defaultValue={block.twitterURL}
+            value={block.twitterURL}
+          />
+        </div>
+        <div style={{ display: "flex" }}>
+          <Button type="Submit">Submit</Button>
+          <Button
+            className="DataOutput__editing--cancel"
+            onClick={() => setEditing(null)}
+          >
+            Cancel
+          </Button>
+        </div>
+      </form>
+      <div></div>
+    </BlockWrapper>
   );
 };
 
