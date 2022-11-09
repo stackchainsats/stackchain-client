46c2f9388a364f986c7af0223f5d106cf1e95e69
Psyduck Spyduck <jc4466@gmail.com>
2022-11-09 11:03:57 -0800

    I made an update to the stackjoin page

--- pages/stackjoin.js
+++ pages/stackjoin.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Checkbox from "../components/Checkbox";
+<<<<<<< Updated upstream
 import Button from "../components/Button";
 import Link from "next/link";
 import axios from "axios";
+=======
+import Button from "../components/Button"
+import TextInputStyles from"../components/TextInput"
+>>>>>>> Stashed changes
 
 const Stackjoin = () => {
   const [stackjoins, setStackjoins] = useState([]);
@@ -28,11 +33,17 @@ const Stackjoin = () => {
 
   return (
     <div>
-      <h1>Stackjoin Page</h1>
+      <h1>Stackjoin Block Builder</h1>
+      <TextInputStyles> Write Your Tweet</TextInputStyles>
+      <span>TwitterHandle - $Ammount - URL - Time Stamp</span>>
       <Checkbox checked={checked} onChange={handleChange} />
+<<<<<<< Updated upstream
       <Link href="/add-stackjoin">
         <Button>Add Stackjoin</Button>
       </Link>
+=======
+      <Button>Text for test</Button>
+>>>>>>> Stashed changes
     </div>
   );
 };
