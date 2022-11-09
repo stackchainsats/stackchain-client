adbe9e694e2b4b75fdba094f62a56fed48196a60
Mason price <TheSJQuant@proton.me>
2022-11-09 12:39:24 -0700

    Weiner

--- pages/statistics.js
+++ pages/statistics.js
@@ -12,9 +12,17 @@ const Statistics = () => {
   return (
     <StatisticsWrapper>
       <h1>Statistics Page</h1>
+      <h2>Cumulative Bitcoin Stacked Onchain</h2>
       <img src = "/Stats_Cumulative_USD.png" />
+      <h2>Unique Accounts Partcipating Onchain</h2>
+      <img src = "/Stats_Unique_Accounts.png" />
+      <h2>Dollars Converted to Bitcoin Per Day</h2>
+      <img src = "/Stats_USD_Converted_Per_Day.png" />
+      <h2>Blocks Stacked Per Day</h2>
+      <img src = "/Stats_Block_Mined_Per_Day.png" />
     </StatisticsWrapper>
   );
 };
 export default Statistics;
+
 
