40dbfb3d64ae2d657aca4c057b4c256a0977a95b
Peter Durham <peterdurham@protonmail.com>
2022-11-09 11:45:24 -0800

    resolved merge conflict

--- pages/stackjoin.js
+++ pages/stackjoin.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Checkbox from "../components/Checkbox";
-<<<<<<< Updated upstream
 import Button from "../components/Button";
 import Link from "next/link";
 import axios from "axios";
-=======
-import Button from "../components/Button"
-import TextInputStyles from"../components/TextInput"
->>>>>>> Stashed changes
+import Button from "../components/Button";
+import TextInputStyles from "../components/TextInput";
 
 const Stackjoin = () => {
   const [stackjoins, setStackjoins] = useState([]);
@@ -37,13 +34,9 @@ const Stackjoin = () => {
       <TextInputStyles> Write Your Tweet</TextInputStyles>
       <span>TwitterHandle - $Ammount - URL - Time Stamp</span>>
       <Checkbox checked={checked} onChange={handleChange} />
-<<<<<<< Updated upstream
       <Link href="/add-stackjoin">
         <Button>Add Stackjoin</Button>
       </Link>
-=======
-      <Button>Text for test</Button>
->>>>>>> Stashed changes
     </div>
   );
 };
--- pages/statistics.js
+++ pages/statistics.js
@@ -1,28 +1,25 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Image from "next/image";
 
-
-const StatisticsWrapper= styled.div`
-img {
-  width: 100%;
-}
-`
-
+const StatisticsWrapper = styled.div`
+  img {
+    width: 100%;
+  }
+`;
 
 const Statistics = () => {
   return (
     <StatisticsWrapper>
       <h1>Statistics Page</h1>
       <h2>Cumulative Bitcoin Stacked Onchain</h2>
-      <img src = "/Stats_Cumulative_USD.png" />
+      <img src="/Stats_Cumulative_USD.png" />
       <h2>Unique Accounts Partcipating Onchain</h2>
-      <img src = "/Stats_Unique_Accounts.png" />
+      <img src="/Stats_Unique_Accounts.png" />
       <h2>Dollars Converted to Bitcoin Per Day</h2>
-      <img src = "/Stats_USD_Converted_Per_Day.png" />
+      <img src="/Stats_USD_Converted_Per_Day.png" />
       <h2>Blocks Stacked Per Day</h2>
-      <img src = "/Stats_Block_Mined_Per_Day.png" />
+      <img src="/Stats_Block_Mined_Per_Day.png" />
     </StatisticsWrapper>
   );
 };
 export default Statistics;
-
-
