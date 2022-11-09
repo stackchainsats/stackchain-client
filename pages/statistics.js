e71c11c5a491edea0a9edaf80039e694e5cca6d9
Mason price <TheSJQuant@proton.me>
2022-11-09 12:15:54 -0700

    Stats

--- pages/statistics.js
+++ pages/statistics.js
@@ -1,8 +1,20 @@
+import styled from "styled-components"
+
+
+const StatisticsWrapper= styled.div`
+img {
+  width: 100%;
+}
+`
+
+
 const Statistics = () => {
   return (
-    <div>
+    <StatisticsWrapper>
       <h1>Statistics Page</h1>
-    </div>
+      <img src = "/Stats_Cumulative_USD.png" />
+    </StatisticsWrapper>
   );
 };
 export default Statistics;
+
