21def5661d0c8c188f453c2c6ea7a4832b22f024
Peter Durham <peterdurham@protonmail.com>
2022-11-09 10:43:08 -0800

    added get mempool route

--- pages/stackjoin.js
+++ pages/stackjoin.js
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Checkbox from "../components/Checkbox";
 import Button from "../components/Button";
 import Link from "next/link";
+import axios from "axios";
 
 const Stackjoin = () => {
+  const [stackjoins, setStackjoins] = useState([]);
   const [checked, setChecked] = useState(false);
+
+  React.useEffect(() => {
+    getStackjoins();
+  }, []);
+
+  const getStackjoins = async () => {
+    // setBlocks(BlocksJSON);
+    // console.log("getBlocks");
+    const res = await axios.get(
+      "https://stackchain-backend.herokuapp.com/api/stackjoins"
+    );
+    const data = res.data;
+    setStackjoins(data);
+  };
 
   const handleChange = (e) => {
     setChecked(e.target.checked);
